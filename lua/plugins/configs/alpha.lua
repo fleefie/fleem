ebd3da54fc90e819316713d82beca476b4bd8ead
fleefie <waally2005@tuta.io>
2024-09-11 17:18:15 +0200

    Implemented configs for alpha and which key

---
+++ lua/plugins/configs/alpha.lua
@@ -0,0 +1,47 @@
+local alpha = require "alpha"
+local dashboard = require "alpha.themes.dashboard"
+local M = {}
+
+function M.setup()
+  local header = [[
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠣⣀⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⢸⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠻⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⡇⠀⡠⠃⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣿⡇⠛⠛⠛⠛⠛⠛⠛⢻⡿⠁⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣶⣶⣶⣶⣶⣭⣥⣤⣤⣤⣤⣤⣤⣤⣬⣥⣤⣀⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣛⣒⣀
+⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢫⣾⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⢻⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⠿⠁⠉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡜⣿⣿⣿
+⠀⠀⠀⠀⠀⠀⠀⡛⠻⣿⣿⣿⣿⠿⠇⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣇⡛⠻⣿⣿⣿⣿⢿⣿⣿⢸⣿⣿
+⠀⠀⠀⠀⠀⠀⢡⣿⣷⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠓⠂⠉⠙⠛⠃⠀⠀⢻⣿⣿
+⠀⠀⠀⠀⠀⡈⣼⣿⣿⠇⢸⣟⠛⠇⠀⠀⠀⠀⠀⠀⠀⢶⡶⠶⢶⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⠀⠶⢰⣿⡿⠏⠀⠸⢿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⣀⣾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⠀⣤⣿⣿⠃⠀⠀⢀⣀⣠⡄⠐⠒⢲⣶⣤⡀⠀⠀⠉⠉⠁⠀⢠⣤⣤⣤⣄⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⠀⣿⣿⡏⢀⣀⣴⣾⣿⣿⣧⣤⣤⣾⣿⣿⣷⣤⡀⠀⠀⠀⠀⠈⠙⠻⠿⠟⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⢰⣿⣿⣿⢸⣿⣿⣏⡹⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣷⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⠘⣿⣿⣿⣆⡘⢿⣿⣿⣮⡻⠿⣿⡿⣛⣵⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿
+⠀⠀⠀⠀⠀⠀⠛⠛⠃⠘⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿
+  ]]
+  dashboard.section.header.val = vim.split(header, "\n")
+
+  -- Menu buttons
+  dashboard.section.buttons.val = {
+    dashboard.button("f", "󰈞  Find files", "<cmd>Telescope find_files<cr>"),
+    dashboard.button("r", "  Recent files", "<cmd>Telescope oldfiles<cr>"),
+    dashboard.button("p", "  Projects", "<cmd>Telescope projects<CR>"),
+    dashboard.button("n", "  New file", "<cmd>ene!<CR>"),
+    dashboard.button("t", "󰊄  Find text", "<cmd>Telescope live_grep<cr>"),
+    dashboard.button("u", "󱐥  Sync plugins", "<cmd>Lazy sync<CR>"),
+    dashboard.button("m", "  Install language tools", "<cmd>Mason<CR>"),
+    dashboard.button("x", "󰅖  Quit", "<cmd>qa<cr>"),
+  }
+
+  -- Fortune footer
+  local fortune = require("fortune").get_fortune()
+  dashboard.section.footer.val = fortune
+
+  alpha.setup(dashboard.opts)
+end
+
+return M
