d3d80107effbb5c3c8e86df7faa0de3a78591cf3
Fleefie <>
2024-11-13 09:59:27 +0100

    RGBDS syntax highlighting, formatting changes

--- init.lua
+++ init.lua
@@ -8,3 +8,14 @@ require("fleem.lazy")
 
 -- Load post-lazy commands. Avoid these like the plague!
 require("fleem.lazy_after")
+
+-- HACK: should be in its own file!
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.asm" },
+  command = "lua vim.api.nvim_command('setfiletype rgbds')"
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.inc" },
+  command = "lua vim.api.nvim_command('setfiletype rgbds')"
+})
--- lua/fleem/plugins/lsp/format.lua
+++ lua/fleem/plugins/lsp/format.lua
@@ -1,40 +1,41 @@
 -- Conform for formatters! Yay.
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				-- Add formatters here.
-				lua = { "stylua" },
-				rust = { "rustfmt" },
-				c = { "clang-format" },
-				cpp = { "clang-format" },
-				latex = { "latexindent" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-		})
+    conform.setup({
+      formatters_by_ft = {
+        -- Add formatters here.
+        lua = { "stylua" },
+        rust = { "rustfmt" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        latex = { "latexindent" },
+        python = { "black" }
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+      default_format_opts = {
+        lsp_format = "fallback",
+      },
+    })
 
-		vim.keymap.set({ "n", "x", "v" }, "<F3>", "<cmd>lua require('conform').format()<cr>")
+    vim.keymap.set({ "n", "x", "v" }, "<F3>", "<cmd>lua require('conform').format()<cr>")
 
-		local wk = require("which-key")
-		wk.add({
-			{
-				"<leader>lf",
-				"<cmd>lua require('conform').format()<cr>",
-				desc = "Format",
-				nowait = true,
-				remap = false,
-			},
-		})
-	end,
+    local wk = require("which-key")
+    wk.add({
+      {
+        "<leader>lf",
+        "<cmd>lua require('conform').format()<cr>",
+        desc = "Format",
+        nowait = true,
+        remap = false,
+      },
+    })
+  end,
 }
